0c7547566938ac9411d70b2cd2fcfb361bed2af3
l2m2 <l2m2lq@gmail.com>
2021-03-01 22:46:22 +0800

    refactor: user model

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -9,9 +9,6 @@ from app.schemas.user import UserCreate, UserUpdate
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
-  def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-    return db.query(User).filter(User.email == email).first()
-
   def get_by_username(self, db: Session, *, username: str) -> Optional[User]:
     return db.query(User).filter(User.username == username)
 
@@ -38,14 +35,6 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
       del update_data["password"]
       update_data["password"] = hashed_password
     return super().update(db, db_obj=db_obj, obj_in=update_data)
-
-  def authenticate_by_email(self, db: Session, *, email: str, password: str) -> Optional[User]:
-    user = self.get_by_email(db, email=email)
-    if not user:
-      return None
-    if not verify_password(password, user.hashed_password):
-      return None
-    return user
 
   def authenticate(self, db: Session, *, username: str, password: str) -> Optional[User]:
     user = self.get_by_username(db, username=username)
