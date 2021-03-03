0aed1998d3448468de2c86fd5717982ea8434822
l2m2 <l2m2lq@gmail.com>
2021-03-03 15:30:25 +0800

    refactor: disable flake errors in files

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
   SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
   @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-  def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+  def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:  # noqa
     if isinstance(v, str):
       return v
     return PostgresDsn.build(
--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -1,4 +1,4 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
-from app.db.base_class import Base
-from app.models.user import User
+from app.db.base_class import Base  # noqa: F401
+from app.models.user import User  # noqa: F401
