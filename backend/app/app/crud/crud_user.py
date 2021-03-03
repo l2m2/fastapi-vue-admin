17c20378c449eb69eb057e0a82ffc4344e735628
l2m2 <l2m2lq@gmail.com>
2021-03-03 15:34:52 +0800

    refactor: rename crud_base to base

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, Optional, Union
 from sqlalchemy.orm import Session
 
 from app.core.security import get_password_hash, verify_password
-from app.crud.crud_base import CRUDBase
+from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 
