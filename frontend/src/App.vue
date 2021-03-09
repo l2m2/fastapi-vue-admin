1a6259b0ea3051d94ffe415f89a0b5c01e62affc
l2m2 <l2m2lq@gmail.com>
2021-03-09 15:17:25 +0800

    feat: add locale-provider

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <a-config-provider :locale="locales[$store.state.app.lang]">
+    <div id="app">
+      <router-view />
+    </div>
+  </a-config-provider>
 </template>
+<script>
+import zhcn from "ant-design-vue/lib/locale-provider/zh_CN";
+import en from "ant-design-vue/lib/locale-provider/en_GB";
+
+export default {
+  data() {
+    return {
+      locales: {
+        zhcn,
+        en
+      }
+    };
+  }
+};
+</script>
 
 <style lang="less"></style>
