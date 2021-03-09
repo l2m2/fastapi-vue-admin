b3f2c4a4306d3d6d12d99f0388a12f86ef8a54b1
l2m2 <l2m2lq@gmail.com>
2021-03-09 15:36:08 +0800

    refactor: module->modules

--- frontend/src/locales/index.js
+++ frontend/src/locales/index.js
@@ -1,8 +1,8 @@
 import common from "./common";
 import app from "./app";
-import admin from "./module/admin";
-import apps from "./module/apps";
-import dash from "./module/dash";
+import admin from "./modules/admin";
+import apps from "./modules/apps";
+import dash from "./modules/dash";
 
 export default {
   _: common,
