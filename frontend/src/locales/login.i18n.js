d3752f631cb68ac389abf1a826ad5d791fa9181f
l2m2 <l2m2lq@gmail.com>
2021-03-16 16:04:15 +0800

    feat: login page ui

---
+++ frontend/src/locales/login.i18n.js
@@ -0,0 +1,10 @@
+export default {
+  username: {
+    placeholder: { en: "Username", zhcn: "用户名" }
+  },
+  password: {
+    placeholder: { en: "Password", zhcn: "密码" }
+  },
+  "remember-me": { en: "Remember me", zhcn: "自动登录" },
+  login: { en: "Sign In", zhcn: "登录" }
+};
--- frontend/src/views/login/Login.vue
+++ frontend/src/views/login/Login.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>Login Page</div>
+  <div class="main">
+    <a-form>
+      <a-form-item>
+        <a-input size="large" type="text" :placeholder="$t('login.username.placeholder')">
+          <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input-password size="large" :placeholder="$t('login.password.placeholder')">
+          <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }" />
+        </a-input-password>
+      </a-form-item>
+    </a-form>
+    <a-form-item>
+      <a-checkbox>{{ $t("login.remember-me") }}</a-checkbox>
+    </a-form-item>
+    <a-form-item style="margin-top:24px">
+      <a-button size="large" type="primary" htmlType="submit" block>{{ $t("login.login") }}</a-button>
+    </a-form-item>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="less" scoped></style>
