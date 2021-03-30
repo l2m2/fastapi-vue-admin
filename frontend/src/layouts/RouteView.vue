3057aca0e9b3ae9917fffac84ab8bb5ef7597365
l2m2 <l2m2lq@gmail.com>
2021-03-30 14:44:58 +0800

    feat: add RouteView

---
+++ frontend/src/layouts/RouteView.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "RouteView",
+  props: {
+    keepAlive: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {};
+  },
+  render() {
+    const {
+      $route: { meta },
+      $store: { getters }
+    } = this;
+    const inKeep = (
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    );
+    const notKeep = <router-view />;
+    if (!getters.multiTab && !meta.keepAlive) {
+      return notKeep;
+    }
+    return this.keepAlive || getters.multiTab || meta.keepAlive ? inKeep : notKeep;
+  }
+};
+</script>
---
+++ frontend/src/layouts/index.js
@@ -0,0 +1,5 @@
+import UserLayout from "./UserLayout";
+import BasicLayout from "./BasicLayout";
+import RouteView from "./RouteView";
+
+export { UserLayout, BasicLayout, RouteView };
